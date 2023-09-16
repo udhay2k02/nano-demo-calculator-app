0b9b6117fd226cb80a5449d64b73d70c5685a53d
udhay2k02 <udhaythallapelli@gmail.com>
2023-09-16 08:31:23 +0530

    added code
    added code

--- server.py
+++ server.py
@@ -5,15 +5,21 @@ app = Flask(__name__)
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello World!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    first=int(input("Enter first number"))
+    second=int(input("Enter second number"))
+
+    return 'first+second'
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    first=int(input("Enter first number"))
+    second=int(input("Enter second number"))
+
+    return 'first-second'
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
